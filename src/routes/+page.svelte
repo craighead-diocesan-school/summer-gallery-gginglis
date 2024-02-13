f316c52ba041e5413a37762fcedf8ce77eb4c9f0
gginglis <inglisge23@craighead.school.nz>
2024-02-13 17:20:12 +1300

    added photos

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,12 @@
 <script>
   import Header from "$lib/Header.svelte";
   import Footer from "$lib/Footer.svelte";
+  import Nav from "$lib/Nav.svelte";
 </script>
 
 <Header />
 
+<Nav />
 <main>
   <h2>SvelteKit</h2>
 
@@ -12,6 +14,9 @@
     Welcome to coding with SvelteKit, a modern JavaScript framework that makes
     it easy to code great apps.
   </p>
+
+  <img src="IMG_3184.JPG" alt="" />
+  <img src="IMG_2556.JPG" alt="" />
 </main>
 
 <Footer />
---
+++ src/routes/about-me/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+    import Header from "$lib/Header.svelte";
+    import Footer from "$lib/Footer.svelte";
+    import Nav from "$lib/Nav.svelte";
+</script>
+
+<Header />
+
+<Nav />
+
+<main>
+    <img src="/amberandg.JPG" alt="" />
+    <img src="/avaandg.JPG" alt="" />
+    <img src="/bffs.JPG" alt="" />
+    <img src="/sc23.JPG" alt="" />
+    <img src="/cuesyrupy.JPG" alt="" />
+</main>
+
+<Footer />
+
+<style>
+    img {
+        height: 250px;
+    }
+</style>
