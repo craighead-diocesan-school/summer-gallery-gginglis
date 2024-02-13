66e70d2dc3a8bc8d5aced6bd2d29b29f16861b96
gginglis <inglisge23@craighead.school.nz>
2024-02-14 12:13:44 +1300

    made personal card

---
+++ src/routes/card.svelte
@@ -0,0 +1,38 @@
+<div class="card">
+    <div class="card-image">
+        <figure class="image is-4by3">
+            <img
+                src="https://bulma.io/images/placeholders/1280x960.png"
+                alt=""
+            />
+        </figure>
+    </div>
+    <!-- .card-image -->
+
+    <div class="card-content">
+        <div class="media">
+            <div class="media-left">
+                <figure class="image is-48x48">
+                    <img
+                        src="https://bulma.io/images/placeholders/96x96.png"
+                        alt=""
+                    />
+                </figure>
+            </div>
+
+            <div class="media-content">
+                <p class="title is-4">Jane Smith</p>
+                <p class="subtitle is-6">@janesmith</p>
+            </div>
+        </div>
+        <!-- .media -->
+
+        <div class="content">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+            nec iaculis mauris.
+        </div>
+        <!-- .content -->
+    </div>
+    <!-- .card-content -->
+</div>
+<!-- .card -->
