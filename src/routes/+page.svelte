69845a176ae2997844a677d215add64b6e0110d9
gginglis <inglisge23@craighead.school.nz>
2024-02-14 12:09:01 +1300

    made a column layout on home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,14 +9,31 @@
 <Nav />
 <main>
   <h2>SvelteKit</h2>
-
-  <p>
-    Welcome to coding with SvelteKit, a modern JavaScript framework that makes
-    it easy to code great apps.
-  </p>
-
-  <img src="IMG_3184.JPG" alt="" />
-  <img src="IMG_2556.JPG" alt="" />
+  <div class="columns">
+    <div class="column">
+      <p>
+        Welcome to coding with SvelteKit, a modern JavaScript framework that
+        makes it easy to code great apps.
+      </p>
+    </div>
+    <div class="column">
+      <h2>I'm G</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto obcaecati
+        voluptates, esse inventore quisquam illo architecto. Voluptatum
+        obcaecati dolorem vero tempore voluptatem voluptas, vel hic reiciendis
+        praesentium quis possimus pariatur!
+      </p>
+    </div>
+    <div class="column">
+      <h2>This is Digi</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed est nemo id
+        sapiente voluptas possimus aut a fugit deleniti iure illum assumenda
+        tempora, unde cumque exercitationem, tenetur eum, quo eveniet.
+      </p>
+    </div>
+  </div>
 </main>
 
 <Footer />
